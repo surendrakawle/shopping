b1406807e00d3741eb5edb63c729cdcaee696565
surendra kawle <kawlesurendra@thewhiteboard.company>
2020-04-09 01:41:51 +0530

    login UI design 09-04-2020 01:41

--- resources/views/admin/users/edit.blade.php
+++ resources/views/admin/users/edit.blade.php
@@ -1,67 +1,120 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-12">
-            <div class="card">
-                <div class="card-header">Edit user {{$user->name}}</div>
-
-                <div class="card-body">
-                    <form action="{{route('admin.users.update',$user)}}" method="POST">
-                   
-                   <div class="form-group row">
-                            <label for="email" class="col-md-1 col-form-label text-md-left">Email</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ $user->email }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="name" class="col-md-1 col-form-label text-md-left">Name</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{$user->name}}" required  autofocus>
-
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                    @csrf
-                    {{method_field('PUT')}}
-                    <div class="form-group row">
-                            <label for="roles" class="col-md-1 col-form-label text-md-left">Roles</label>
-                             <div class="col-md-6">
-                                    @foreach($roles as $role)
-                                    <div class="form-check ">
-                                    <label class="container1"> {{$role->name}}
-                                        <input type="checkbox" name="role[]" value="{{$role->id}}"
-                                        @if($user->roles()->pluck('roles.id')->contains($role->id)) checked @endif >
-                                        <span class="checkmark"></span>
-                                    </label>
+<div class="card">
+                        <div class="header">
+                            <div class="row clearfix">
+                                <div class="col-xs-12 col-sm-6">
+                                    <h2>Edit user {{$user->name}}</h2>
+                                </div>
+                                <div class="col-xs-12 col-sm-6 align-right">
+                                    <div class="switch panel-switch-btn">
+                                        <span class="m-r-10 font-12">REAL TIME</span>
+                                        <label>OFF<input type="checkbox" id="realtime" checked><span class="lever switch-col-cyan"></span>ON</label>
                                     </div>
-                                    @endforeach
+                                </div>
                             </div>
+                            <ul class="header-dropdown m-r--5">
+                                <li class="dropdown">
+                                    <a href="javascript:void(0);" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                                        <i class="material-icons">more_vert</i>
+                                    </a>
+                                    <ul class="dropdown-menu pull-right">
+                                        <li><a href="javascript:void(0);">Action</a></li>
+                                        <li><a href="javascript:void(0);">Another action</a></li>
+                                        <li><a href="javascript:void(0);">Something else here</a></li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="body">
+                           <div class="card-body table-responsive-sm table-responsive-md table-responsive-lg">
+                
+                                <form action="{{route('admin.users.update',$user)}}" class="form-horizontal" method="POST">
+                   
+                                <div class="row clearfix">
+
+                                    <div class="col-lg-2 col-md-2 col-sm-4 col-xs-5 form-control-label">
+                                           <label for="email">Email Address</label>
+                                    </div>
+                                     <div class="col-lg-10 col-md-10 col-sm-8 col-xs-7">
+                                           <div class="form-group">
+                                               <div class="form-line">
+                                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ $user->email }}" required autocomplete="email" autofocus>
+
+                                                   @error('email')
+                                                       <span class="invalid-feedback" role="alert">
+                                                           <strong>{{ $message }}</strong>
+                                                       </span>
+                                                   @enderror   
+                                                </div>
+                                           </div>
+                                       </div>
+                           
+                                </div>
+                                <div class="row clearfix">
+
+                                    <div class="col-lg-2 col-md-2 col-sm-4 col-xs-5 form-control-label">
+                                           <label for="Name">Name</label>
+                                    </div>
+                                     <div class="col-lg-10 col-md-10 col-sm-8 col-xs-7">
+                                           <div class="form-group">
+                                               <div class="form-line">
+                                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{$user->name}}" required  autofocus>
+
+                                                 @error('name')
+                                                     <span class="invalid-feedback" role="alert">
+                                                         <strong>{{ $message }}</strong>
+                                                     </span>
+                                                 @enderror
+                                                </div>
+                                           </div>
+                                       </div>
+                           
+                                </div>
+                                 @csrf
+                                    {{method_field('PUT')}}
+                                <div class="row clearfix">
+
+                                    <div class="col-lg-2 col-md-2 col-sm-4 col-xs-5 form-control-label">
+                                           <label for="Roles">Roles</label>
+                                    </div>
+                                     <div class="col-lg-10 col-md-10 col-sm-8 col-xs-7">
+                                           <div class="form-group">
+                                               <div >
+                                                 @foreach($roles as $role)
+                                                <div class="form-check ">
+                                                <label class="container1"> {{$role->name}}
+                                                    <input type="checkbox" name="role[]" value="{{$role->id}}"
+                                                    @if($user->roles()->pluck('roles.id')->contains($role->id)) checked @endif >
+                                                    <span class="checkmark"></span>
+                                                </label>
+                                                </div>
+                                                @endforeach
+                                                </div>
+                                           </div>
+                                       </div>
+                                </div>
+                                <div class="row clearfix">
+
+                                    <div class="col-lg-2 col-md-2 col-sm-4 col-xs-5 form-control-label">
+                                          
+                                    </div>
+                                     <div class="col-lg-10 col-md-10 col-sm-8 col-xs-7">
+                                           <div class="form-group">
+                                                <button class="btn btn-md btn-primary">Update</button>
+                                         
+                                         <a href=""> 
+                                                <button class="btn btn-md btn-danger">Cancel</button> 
+                                         </a> 
+                                           </div>
+                                       </div>
+                                </div>
+                                </form>   
+                
+                        </div>
+            
+                        </div>
                     </div>
 
-                        <button class="btn btn-md btn-primary">Update</button>   
-                 </form>
-                
-                   
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
 @endsection
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,111 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+<div class="card">
+                        <div class="header">
+                            <div class="row clearfix">
+                                <div class="col-xs-12 col-sm-6">
+                                    <h2>{{ __('Login') }}</h2>
+                                </div>
                             </div>
+                            <ul class="header-dropdown m-r--5">
+                                <li class="dropdown">
+                                    <a href="javascript:void(0);" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                                        <i class="material-icons">more_vert</i>
+                                    </a>
+                                    <ul class="dropdown-menu pull-right">
+                                        <li><a href="javascript:void(0);">Action</a></li>
+                                        <li><a href="javascript:void(0);">Another action</a></li>
+                                        <li><a href="javascript:void(0);">Something else here</a></li>
+                                    </ul>
+                                </li>
+                            </ul>
                         </div>
+                        <div class="body">
+                           <div class="card-body table-responsive-sm table-responsive-md table-responsive-lg">
+                
+                                <form method="POST" class="form-horizontal" action="{{ route('login') }}">
+                                    @csrf
+                                <div class="row clearfix">
 
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                                    <div class="col-lg-2 col-md-2 col-sm-4 col-xs-5 form-control-label">
+                                          <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                                    </div>
+                                     <div class="col-lg-10 col-md-10 col-sm-8 col-xs-7">
+                                           <div class="form-group">
+                                               <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                                                @error('email')
+                                                    <span class="invalid-feedback" role="alert">
+                                                        <strong>{{ $message }}</strong>
+                                                    </span>
+                                                @enderror
+                                           </div>
+                                       </div>
+                           
+                                </div>
+                                <div class="row clearfix">
 
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
+                                    <div class="col-lg-2 col-md-2 col-sm-4 col-xs-5 form-control-label">
+                                          <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                                    </div>
+                                     <div class="col-lg-10 col-md-10 col-sm-8 col-xs-7">
+                                           <div class="form-group">
+                                               <div class="form-line">
+                                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
 
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                                                    @error('password')
+                                                        <span class="invalid-feedback" role="alert">
+                                                            <strong>{{ $message }}</strong>
+                                                        </span>
+                                                    @enderror
+                            
+                                                </div>
+                                           </div>
+                                       </div>
+                           
+                                </div>
+                                <div class="row clearfix">
+
+                                    <div class="col-lg-2 col-md-2 col-sm-4 col-xs-5 form-control-label">
+                                          
+                                    </div>
+                                     <div class="col-lg-10 col-md-10 col-sm-8 col-xs-7">
+                                           <div class="form-group">
+                                               <div >
+                                                 <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
                                     <label class="form-check-label" for="remember">
                                         {{ __('Remember Me') }}
                                     </label>
+                                                </div>
+                                           </div>
+                                       </div>
                                 </div>
-                            </div>
-                        </div>
+                                <div class="row clearfix">
 
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
+                                    <div class="col-lg-2 col-md-2 col-sm-4 col-xs-5 form-control-label">
+                                          
+                                    </div>
+                                     <div class="col-lg-10 col-md-10 col-sm-8 col-xs-7">
+                                           <div class="form-group">
+                                                <button type="submit" class="btn btn-primary">
+                                                     {{ __('Login') }}
+                                                 </button>
 
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
+                                                 @if (Route::has('password.request'))
+                                                     <a class="btn btn-link" href="{{ route('password.request') }}">
+                                                         {{ __('Forgot Your Password?') }}
+                                                     </a>
+                                                 @endif
+                                           </div>
+                                       </div>
+                                </div>
+                                </form>   
+                
                         </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+            
+                        </div>
+                    </div>
+
 @endsection
